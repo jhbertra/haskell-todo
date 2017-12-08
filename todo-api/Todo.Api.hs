d6c8eb7df31dd1823d847c211c46892d083dbaed
Jamie Bertram <jamie@novoideation.ca>
2017-12-07 23:51:15 -0700

    Add Get Request / Response
    - Introduce Id type to handle either string or int ids
    - For now, make the get response a maybe

--- todo-api/Todo.Api.hs
+++ todo-api/Todo.Api.hs
@@ -1,8 +1,21 @@
 module Todo.Api where
 
-  data Request = ListRequest deriving Show
+  data Id 
+    = Uid Int 
+    | Name String deriving Show
 
-  newtype Response = ListResponse [String] deriving Show
+  data Request 
+    = ListRequest 
+    | GetRequest Id deriving Show
+
+  data Response 
+    = ListResponse [String]
+    | GetResponse (Maybe String) deriving Show
 
   handleRequest :: Request -> Response
   handleRequest ListRequest = ListResponse ["foo", "bar", "squid"]
+  handleRequest (GetRequest id) =
+    GetResponse $
+      case id of
+        Uid i -> if i == 42 then Just "foo" else Nothing
+        Name n -> if n == "foo" then Just "foo" else Nothing
