44f779f4dc527bc4d2663b96cd7cd41bbcb0d532
Jamie Bertram <jamie@novoideation.ca>
2017-12-08 00:21:19 -0700

    Add todo record

--- todo-api/Todo.Api.hs
+++ todo-api/Todo.Api.hs
@@ -1,25 +1,59 @@
 module Todo.Api where
 
-  data Id 
-    = Uid Int 
+
+  data Todo = Todo
+    { uid :: Int
+    , name :: String
+    , description :: String
+    , completed :: Bool
+    } deriving Show
+
+
+  data Id
+    = Uid Int
     | Name String deriving Show
 
-  data Request 
-    = ListRequest 
+
+  data Request
+    = ListRequest
     | GetRequest Id deriving Show
 
-  data Response 
-    = ListResponse [String]
-    | GetResponse String deriving Show
+
+  data Response
+    = ListResponse [Todo]
+    | GetResponse Todo deriving Show
+
 
   newtype Error = TodoNotFound Id deriving Show
 
+
   handleRequest :: Request -> Either Error Response
-  handleRequest ListRequest = Right $ ListResponse ["foo", "bar", "squid"]
+  handleRequest ListRequest =
+      Right $
+        ListResponse
+          [ Todo
+            { uid = 1
+            , name = "foo"
+            , description = "Something about foo"
+            , completed = False
+            }
+          ]
+
   handleRequest (GetRequest id) =
     do
-      { todo <- case id of  Uid i | i == 42 -> Right "foo"
-                            Name n | n == "foo" -> Right "foo"
-                            _ -> Left $ TodoNotFound id
+      { todo <-
+        if case id of
+          Uid i | i == 42 -> True
+          Name n | n == "foo" -> True
+          _ -> False
+        then
+          Right Todo
+            { uid = 42
+            , name = "foo"
+            , description = "Something about foo"
+            , completed = False
+            }
+        else
+          Left $ TodoNotFound id
       ; return $ GetResponse todo
       }
