6a1e7dbbb3ec269231143b48e6f49e5f869701c4
Jamie Bertram <jhbertra@gmail.com>
2017-12-14 13:39:23 +0100

    Start HTTP request parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
 import Web
+import Http
 
 main :: IO ()
-main = runServer $ \msg -> return msg
-    
+--main = runServer $ \msg -> return msg
+main = print $ parseRequest "line\r\nheader1\r\nheader2\r\nbody"
--- lib/Http.hs
+++ lib/Http.hs
@@ -1,4 +1,38 @@
-module Http where
+module Http
+  ( Request
+  , parseRequest
+  ) where
 
-import Text.Parsec
+import qualified Text.Parsec as Parsec
+import Text.Parsec ((<?>))
+import Control.Applicative
+import Control.Monad.Identity (Identity)
 
+data Request = Request String [String] String deriving Show
+
+
+requestLine :: Parsec.Parsec String () String
+requestLine = Parsec.many1 <$> Parsec.noneOf $ ['\r','\n']
+
+
+header :: Parsec.Parsec String () String
+header = Parsec.many1 <$> Parsec.noneOf $ ['\r','\n']
+
+
+crlf :: Parsec.Parsec String () String
+crlf = Parsec.string "\r\n" <?> "CRLF"
+
+
+body :: Parsec.Parsec String () String
+body = Parsec.many Parsec.anyChar
+
+
+request :: Parsec.Parsec String () Request
+request = Request 
+  <$> ((requestLine <?> "Request Line") <* crlf) 
+  <*> (Parsec.many ((header <?> "Request Header") <* crlf))
+  <*> (crlf *> body <?> "Request Body")
+
+
+parseRequest :: String -> Either Parsec.ParseError Request
+parseRequest = Parsec.parse request "(HttpRequest)"
