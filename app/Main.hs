9c7527ead43b9da5eecde6b8c8a1af37cf05cfbf
Jamie Bertram <jhbertra@gmail.com>
2017-12-14 14:03:36 +0100

    Parse request line

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,8 @@ module Main where
 
 import Web
 import Http
+import System.Environment
 
 main :: IO ()
 --main = runServer $ \msg -> return msg
-main = print $ parseRequest "line\r\nheader1\r\nheader2\r\nbody"
+main = print $ parseRequest "GET asdf 12\r\nheader1\r\nheader2\r\n\r\nbody"
--- lib/Http.hs
+++ lib/Http.hs
@@ -8,19 +8,42 @@ import Text.Parsec ((<?>))
 import Control.Applicative
 import Control.Monad.Identity (Identity)
 
-data Request = Request String [String] String deriving Show
+
+data RequestLine = RequestLine String String String deriving Show
 
 
-requestLine :: Parsec.Parsec String () String
-requestLine = Parsec.many1 <$> Parsec.noneOf $ ['\r','\n']
+data Request = Request RequestLine [String] String deriving Show
 
 
-header :: Parsec.Parsec String () String
-header = Parsec.many1 <$> Parsec.noneOf $ ['\r','\n']
+space :: Parsec.Parsec String () String 
+space  = Parsec.string " " <?> "A Single Space"
 
 
 crlf :: Parsec.Parsec String () String
 crlf = Parsec.string "\r\n" <?> "CRLF"
+
+
+method :: Parsec.Parsec String () String
+method = Parsec.many1 Parsec.letter
+
+
+requestTarget :: Parsec.Parsec String () String
+requestTarget = Parsec.many1 $ Parsec.noneOf [' ']
+
+
+httpVersion :: Parsec.Parsec String () String
+httpVersion = Parsec.many1 $ Parsec.noneOf [' ', '\r', '\n']
+
+
+requestLine :: Parsec.Parsec String () RequestLine
+requestLine = RequestLine 
+  <$> (method <* space)
+  <*> (requestTarget <* space)
+  <*> (httpVersion <* crlf)
+
+
+header :: Parsec.Parsec String () String
+header = Parsec.many1 $ Parsec.noneOf ['\r','\n']
 
 
 body :: Parsec.Parsec String () String
@@ -29,7 +52,7 @@ body = Parsec.many Parsec.anyChar
 
 request :: Parsec.Parsec String () Request
 request = Request 
-  <$> ((requestLine <?> "Request Line") <* crlf) 
+  <$> (requestLine <?> "Request Line")
   <*> (Parsec.many ((header <?> "Request Header") <* crlf))
   <*> (crlf *> body <?> "Request Body")
 
